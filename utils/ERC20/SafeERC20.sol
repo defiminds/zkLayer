2c09eec78dae67c7320a8cacec75b5fd85a60a34
DeFiMinds <108605828+defiminds@users.noreply.github.com>
2023-03-27 10:36:32 +0000

    Update SafeERC20.sol

--- utils/ERC20/SafeERC20.sol
+++ utils/ERC20/SafeERC20.sol
@@ -10,12 +10,7 @@ library SafeERC20 {
     function safeTransfer(IERC20 token, address to, uint256 value) internal {
         require(token.transfer(to, value), "SafeERC20: transfer failed");
     }
-    function safeTransferFrom(
-        IERC20 token,
-        address from,
-        address to,
-        uint256 value
-    ) internal {
+    function safeTransferFrom(IERC20 token, address from, address to, uint256 value) internal {
         require(token.transferFrom(from, to, value), "SafeERC20: transferFrom failed");
     }
     function safeApprove(IERC20 token, address spender, uint256 value) internal {
