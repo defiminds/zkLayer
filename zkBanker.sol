3c3fdf20b105ef2673e56e0193b04fcbd479bdbe
DeFiMinds <108605828+defiminds@users.noreply.github.com>
2023-03-27 09:51:12 +0000

    support to wmatic,weth,wbtc and others tokens

--- zkBanker.sol
+++ zkBanker.sol
@@ -1,92 +1,61 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0 <0.9.0;
 
-import "./interfaces/ZKL/IZkSync.sol";
+pragma solidity ^0.8.0;
+
 import "./interfaces/ERC20/IERC20.sol";
-import "./utils/ERC20/SafeERC20.sol";
-import "./utils/math/SafeMath.sol";
+import "./access/Ownable.sol";
+import "./interfaces/ZKL/IZkSync.sol";
 
-contract zkBanker {
-    using SafeMath for uint256;
-    using SafeERC20 for IERC20;
+contract zkBanker is Ownable {
+    mapping(address => mapping(address => uint256)) private balances;
+    mapping(bytes32 => bool) private processedExits;
 
-    address public immutable zkSyncAddress;
-    address public immutable maticAddress;
-    address public immutable wbtcAddress;
+    event Deposit(address indexed user, address indexed token, uint256 amount, uint64 accountId);
+    event Withdrawal(address indexed user, address indexed token, uint256 amount, uint64 accountId);
 
-    mapping(address => mapping(address => uint256)) public balances;
+    constructor(address _zkSyncAddress) Ownable(_zkSyncAddress) {}
 
-    event Deposit(address indexed user, address indexed token, uint256 amount);
-    event Withdraw(address indexed user, address indexed token, uint256 amount);
-    event Transfer(address indexed from, address indexed to, address indexed token, uint256 amount);
-
-    constructor(address _zkSyncAddress, address _maticAddress, address _wbtcAddress) {
-        zkSyncAddress = _zkSyncAddress;
-        maticAddress = _maticAddress;
-        wbtcAddress = _wbtcAddress;
-    }
-
-    function deposit(address token, uint256 amount) external {
-        IZkSync zkSync = IZkSync(zkSyncAddress);
-        require(token == maticAddress || token == wbtcAddress, "Invalid token");
-
-        IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
-        IERC20(token).safeApprove(zkSyncAddress, amount);
-        uint64 accountId = zkSync.getAccountId(msg.sender);
-
-        if (accountId == 0) {
-            accountId = zkSync.createAccount();
-            zkSync.setAccountOnchainPubkey(accountId);
-            zkSync.setAccountCommitment(accountId);
-        }
-
-        zkSync.depositERC20(token, accountId, amount);
-        balances[msg.sender][token] = balances[msg.sender][token].add(amount);
-
-        emit Deposit(msg.sender, token, amount);
-    }
-
-    function withdraw(address token, uint256 amount) external {
-        IZkSync zkSync = IZkSync(zkSyncAddress);
-        require(token == maticAddress || token == wbtcAddress, "Invalid token");
+    function deposit(address token, uint256 amount) public {
+        IERC20(token).transferFrom(msg.sender, address(this), amount);
 
         uint64 accountId = zkSync.getAccountId(msg.sender);
-        require(accountId != 0, "Account not found");
 
-        zkSync.withdrawERC20(token, accountId, amount);
-        IERC20(token).safeTransfer(msg.sender, amount);
-        balances[msg.sender][token] = balances[msg.sender][token].sub(amount);
+        IERC20(token).approve(zkSyncAddress, amount);
+        zkSync.depositERC20(token, amount, accountId);
 
-        emit Withdraw(msg.sender, token, amount);
+        balances[msg.sender][token] += amount;
+
+        emit Deposit(msg.sender, token, amount, accountId);
     }
 
-    function transfer(address token, address to, uint256 amount) external {
-        IZkSync zkSync = IZkSync(zkSyncAddress);
-        require(token == maticAddress || token == wbtcAddress, "Invalid token");
+    function withdraw(address token, uint256 amount) public {
+        require(balances[msg.sender][token] >= amount, "zkBanker: insufficient balance");
 
         uint64 accountId = zkSync.getAccountId(msg.sender);
-        require(accountId != 0, "Account not found");
 
-        zkSync.transferERC20(token, accountId, to, amount);
-        balances[msg.sender][token] = balances[msg.sender][token].sub(amount);
-        balances[to][token] = balances[to][token].add(amount);
+        bytes32 _transferId = zkSync.withdrawPendingBalance(token, amount, accountId);
+        processedExits[_transferId] = true;
 
-        emit Transfer(msg.sender, to, token, amount);
+        balances[msg.sender][token] -= amount;
+
+        emit Withdrawal(msg.sender, token, amount, accountId);
     }
 
     function getBalance(address user, address token) public view returns (uint256) {
-        IZkSync zkSync = IZkSync(zkSyncAddress);
         uint64 accountId = zkSync.getAccountId(user);
-
-        if (accountId != 0) {
-            return zkSync.getBalanceToWithdraw(accountId, token).add(balances[user][token]);
-        } else {
-            return balances[user][token];
-        }
+        return zkSync.getBalance(accountId, token);
     }
 
-    function withdrawAll(address token) external {
-        uint256 balance = getBalance(msg.sender, token);
-        withdraw(token, balance);
+    function balanceOf(address user, address token) public view returns (uint256) {
+        return balances[user][token] + getBalance(user, token);
+    }
+
+    function processExits(bytes32[] calldata _transferIds) public onlyOwner {
+        for (uint256 i = 0; i < _transferIds.length; i++) {
+            if (!processedExits[_transferIds[i]]) {
+                zkSync.processExodusModeExit(_transferIds[i]);
+                processedExits[_transferIds[i]] = true;
+            }
+        }
     }
 }
