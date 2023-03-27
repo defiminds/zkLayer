2ec8e2e7ec4c4531fb4ec611b0ae503b37c9d81e
DeFiMinds <108605828+defiminds@users.noreply.github.com>
2023-03-27 10:33:55 +0000

    Update IERC20.sol

--- interfaces/ERC20/IERC20.sol
+++ interfaces/ERC20/IERC20.sol
@@ -10,11 +10,7 @@ interface IERC20 {
     function transfer(address recipient, uint256 amount) external returns (bool);
     function allowance(address owner, address spender) external view returns (uint256);
     function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) external returns (bool);
+    function transferFrom(address sender,address recipient,uint256 amount) external returns (bool);
     function deposit(uint256 amount) external;
     function withdraw(uint256 amount) external;
 }
